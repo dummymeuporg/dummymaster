34ec90a48941d27f91d7e0cdbb035f8560bcf8f6
Geoffrey ROYER <geoffrey.royer@gmail.com>
2018-09-25 00:38:22 +0200

    Update

--- src/player_state/initial_state.cpp
+++ src/player_state/initial_state.cpp
@@ -1,3 +1,9 @@
+#define BOOST_LOG_DYN_LINK 1
+#include <string>
+
+#include <boost/log/trivial.hpp>
+
+#include "crypto/sha512.hpp"
 #include "player_state/initial_state.hpp"
 
 PlayerState::InitialState::InitialState(::PlayerSession& session)
@@ -9,5 +15,17 @@ PlayerState::InitialState::InitialState(::PlayerSession& session)
 void PlayerState::InitialState::onRead(
     const std::vector<std::uint8_t>& buffer)
 {
+    if (buffer.size() < SHA512_DIGEST_LENGTH)
+    {
+        // XXX: Throw an error somehow
+        return;
+    }
+
+    std::string tagname(reinterpret_cast<const char*>(buffer.data()),
+                        size_t(buffer.size() - SHA512_DIGEST_LENGTH));
+    const std::uint8_t* hash = buffer.data()
+        + buffer.size()
+        - SHA512_DIGEST_LENGTH;
+    BOOST_LOG_TRIVIAL(debug) << "Tagname is: " << tagname;
 
 }
